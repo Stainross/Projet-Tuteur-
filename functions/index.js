8ec03e907a12d9cb2172af8265b22e45626caad6
Sofyann <safsaf.sofyann@gmail.com>
2020-12-27 00:06:44 +0100

    CORS policy

--- functions/index.js
+++ functions/index.js
@@ -81,12 +81,26 @@ const express = require('express');
 var db = require("../src/app/database.js")
 const PORT = 3000;
 const app = express();
-
 /* JSON body parse*/
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://projet-tuteure-42fc0.web.app');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/api/users', (req, res, next) => {
 console.info('/api/users call success ');
 //res.send('Utilisateurs');
